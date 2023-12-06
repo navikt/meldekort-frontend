e4594a1d2ce34890ae6f4fc2564c12d331a0236d
Igor Weber <igor.shuliakov@nav.no>
2023-12-06 14:33:02 +0100

    Fikset env-type for Dekoratøren

--- app/dekorator/dekorator.server.ts
+++ app/dekorator/dekorator.server.ts
@@ -1,10 +1,10 @@
-import { type DecoratorFetchProps } from "@navikt/nav-dekoratoren-moduler/ssr";
+import { type DecoratorEnvProps, type DecoratorFetchProps } from "@navikt/nav-dekoratoren-moduler/ssr";
 import { fetchDecoratorHtml } from "@navikt/nav-dekoratoren-moduler/ssr/index.js";
 
 export async function hentDekoratorHtml() {
 
   const config: DecoratorFetchProps = {
-    env: process.env.DEKORATOR_MILJO ?? "localhost",
+    env: (process.env.DEKORATOR_MILJO ?? "localhost") as DecoratorEnvProps["env"],
     localUrl: "http://localhost:3000/dekorator",
     params: {
       simple: false,
