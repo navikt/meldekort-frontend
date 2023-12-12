800fee1e4d6a66cbcf7db0aad7c2be6c07b5950f
Igor Weber <igor.shuliakov@nav.no>
2023-12-12 15:26:46 +0100

    Om meldekort

--- app/routes/om-meldekort.tsx
+++ app/routes/om-meldekort.tsx
@@ -1,17 +1,52 @@
 import type { MetaFunction } from "@remix-run/node";
 import MeldekortHeader from "~/components/meldekortHeader/MeldekortHeader";
+import Sideinnhold from "~/components/sideinnhold/Sideinnhold";
+import { BodyLong, GuidePanel } from "@navikt/ds-react";
+import { useTranslation } from "react-i18next";
+import { formatHtmlMessage } from "~/utils/intlUtils";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Meldekort" },
-    { name: "description", content: "Om meldekort" },
+    { name: "description", content: "Generelt om meldekort" },
   ];
 };
 
 export default function OmMeldekort() {
+  const { t } = useTranslation();
+
+  const innhold = <GuidePanel poster>
+    <BodyLong>
+      {t("genereltOmMeldekort.velkommen")}
+    </BodyLong>
+    <BodyLong>
+      {t("genereltOmMeldekort.velge")}
+      <ul>
+        <li>
+          {t("genereltOmMeldekort.valg.sende")}
+        </li>
+        <li>
+          {t("genereltOmMeldekort.valg.tidligere")}
+        </li>
+      </ul>
+    </BodyLong>
+    <BodyLong>
+      {formatHtmlMessage(t("genereltOmMeldekort.om.meldekort"),
+        [
+          "https://www.nav.no",
+          t("genereltOmMeldekort.informasjonOmMeldekortLink").trim()
+        ]
+      )}
+    </BodyLong>
+    <BodyLong>
+      {t("genereltOmMeldekort.oss")}
+    </BodyLong>
+  </GuidePanel>
+
   return (
     <div>
       <MeldekortHeader />
+      <Sideinnhold tittel={t("overskrift.genereltOmMeldekort")} innhold={innhold} />
     </div>
   );
 }
