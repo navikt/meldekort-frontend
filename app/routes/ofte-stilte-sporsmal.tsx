7fa0685e6957bba3ce5c3bef990ed4be85bf4645
Igor Weber <igor.shuliakov@nav.no>
2023-12-13 10:08:34 +0100

    Ofte stilte spørsmål

--- app/routes/ofte-stilte-sporsmal.tsx
+++ app/routes/ofte-stilte-sporsmal.tsx
@@ -1,5 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
 import MeldekortHeader from "~/components/meldekortHeader/MeldekortHeader";
+import { useTranslation } from "react-i18next";
+import { Accordion } from "@navikt/ds-react";
+import { formatHtmlMessage } from "~/utils/intlUtils";
+import Sideinnhold from "~/components/sideinnhold/Sideinnhold";
+import sporrende from "~/img/sporrende.svg"
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,9 +14,58 @@ export const meta: MetaFunction = () => {
 };
 
 export default function OfteStilteSporsmal() {
+  const { t } = useTranslation();
+
+  const innhold = <div>
+    <img src={sporrende} className="img" alt="" />
+    <Accordion>
+      <Accordion.Item key="1">
+        <Accordion.Header>
+          {t("oss.sende.overskrift")}
+        </Accordion.Header>
+        <Accordion.Content>
+          {formatHtmlMessage(t("oss.sende.tekst"))}
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item key="2">
+        <Accordion.Header>
+          {t("oss.frist.overskrift")}
+        </Accordion.Header>
+        <Accordion.Content>
+          {formatHtmlMessage(t("oss.frist.tekst"))}
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item key="3">
+        <Accordion.Header>
+          {t("oss.korrigere.overskrift")}
+        </Accordion.Header>
+        <Accordion.Content>
+          {formatHtmlMessage(t("oss.korrigere.tekst"))}
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item key="4">
+        <Accordion.Header>
+          {t("oss.pengene.overskrift")}
+        </Accordion.Header>
+        <Accordion.Content>
+          {formatHtmlMessage(t("oss.pengene.tekst"))}
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item key="5">
+        <Accordion.Header>
+          {t("oss.utbetalt.overskrift")}
+        </Accordion.Header>
+        <Accordion.Content>
+          {formatHtmlMessage(t("oss.utbetalt.tekst"))}
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion>
+  </div>
+
   return (
     <div>
       <MeldekortHeader />
+      <Sideinnhold tittel={t("overskrift.ofteStilteSporsmal")} innhold={innhold} />
     </div>
   );
 }
