7e24481592a4eb3c807c8d0589aab5018a92a91c
Igor Weber <igor.shuliakov@nav.no>
2023-12-15 16:01:53 +0100

    Nye datoUtils

---
+++ app/routes/tidligere-meldekort_.tsx
@@ -0,0 +1,131 @@
+import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import MeldekortHeader from "~/components/meldekortHeader/MeldekortHeader";
+import Sideinnhold from "~/components/sideinnhold/Sideinnhold";
+import { useTranslation } from "react-i18next";
+import { formatHtmlMessage } from "~/utils/intlUtils";
+import { Alert, BodyLong, Table, Tag } from "@navikt/ds-react";
+import type { ReactElement } from "react";
+import { formaterDato, formaterPeriodeDato, formaterPeriodeTilUkenummer } from "~/utils/datoUtils";
+import type { IMeldekort } from "~/models/meldekort";
+import { hentHistoriskeMeldekort } from "~/models/meldekort";
+import { finnRiktigTagVariant, formaterBelop, mapKortStatusTilTekst } from "~/utils/miscUtils";
+import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import type { ISkrivemodus } from "~/models/skrivemodus";
+import { hentSkrivemodus } from "~/models/skrivemodus";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Meldekort" },
+    { name: "description", content: "Tidligere meldekort" },
+  ];
+};
+
+export async function loader() {
+  let skrivemodus: ISkrivemodus | null = null;
+  let historiskeMeldekort: IMeldekort[] | null = null;
+  let feil = false;
+
+  const skrivemodusResponse = await hentSkrivemodus();
+  const historiskeMeldekortResponse = await hentHistoriskeMeldekort();
+
+  if (!skrivemodusResponse.ok) {
+    feil = true
+  } else {
+    skrivemodus = await skrivemodusResponse.json();
+  }
+
+  if (!historiskeMeldekortResponse.ok) {
+    feil = true
+  } else {
+    historiskeMeldekort = await historiskeMeldekortResponse.json();
+  }
+
+  return json({ feil, skrivemodus, historiskeMeldekort });
+}
+
+export default function TidligereMeldekort() {
+  const { i18n, t } = useTranslation();
+
+  // Hent skrivemodus
+  // Hent historiske meldekort
+  // Hvis det er feil, vis feilmelding
+  // Hvis skrivemodus ikke er OK, vis infomelding
+  // Hvis historiske meldekort ikke er hentet, vis advarselsmelding
+  // Hvis historiske meldekort er hentet, vis data
+
+  const { feil, skrivemodus, historiskeMeldekort } = useLoaderData<typeof loader>();
+
+  let innhold: ReactElement
+
+  if (feil) {
+    innhold = <Alert variant="error">{formatHtmlMessage(t("feilmelding.baksystem"))}</Alert>
+  } else if (skrivemodus?.skrivemodus !== true) {
+    // Hvis skrivemodues ikke er OK (ikke true):
+    // Hvis det finnes infomelding i Skrivemodus, vis denne meldingen
+    // Ellers vis standard melding
+    let melding = t("skrivemodusInfomelding")
+    if (!!skrivemodus && !!skrivemodus.melding) {
+      melding = i18n.language === "nb" ? skrivemodus.melding.norsk : skrivemodus.melding.engelsk;
+    }
+
+    innhold = <Alert variant="info">{formatHtmlMessage(melding)}</Alert>
+  } else if (!historiskeMeldekort || historiskeMeldekort.length === 0) {
+    innhold = <Alert variant="warning">{formatHtmlMessage(t("tidligereMeldekort.harIngen"))}</Alert>
+  } else {
+    innhold = <div>
+      <BodyLong spacing>
+        {formatHtmlMessage(t("tidligereMeldekort.forklaring"))}
+      </BodyLong>
+      <BodyLong spacing>
+        {formatHtmlMessage(t("tidligereMeldekort.forklaring.korrigering"))}
+      </BodyLong>
+      <Table zebraStripes>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell scope="col">{t("overskrift.periode")}</Table.HeaderCell>
+            <Table.HeaderCell scope="col">{t("overskrift.dato")}</Table.HeaderCell>
+            <Table.HeaderCell scope="col">{t("overskrift.mottatt")}</Table.HeaderCell>
+            <Table.HeaderCell scope="col">{t("overskrift.status")}</Table.HeaderCell>
+            <Table.HeaderCell scope="col">{t("overskrift.bruttoBelop")}</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {historiskeMeldekort.map((meldekort) => {
+            return (
+              <Table.Row key={meldekort.meldekortId}>
+                <Table.DataCell>
+                  <NavLink to={"/tidligere-meldekort/" + meldekort.meldekortId}>
+                    {t("overskrift.uke")} {formaterPeriodeTilUkenummer(meldekort.meldeperiode.fra, meldekort.meldeperiode.til)}
+                  </NavLink>
+                </Table.DataCell>
+                <Table.DataCell>
+                  {formaterPeriodeDato(meldekort.meldeperiode.fra, meldekort.meldeperiode.til)}
+                </Table.DataCell>
+                <Table.DataCell>
+                  {formaterDato(meldekort.mottattDato)}
+                </Table.DataCell>
+                <Table.DataCell>
+                  <Tag variant={finnRiktigTagVariant(meldekort.kortStatus)}>
+                    {mapKortStatusTilTekst(t, meldekort.kortStatus)}
+                  </Tag>
+                </Table.DataCell>
+                <Table.DataCell>
+                  {formaterBelop(meldekort.bruttoBelop)}
+                </Table.DataCell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  }
+
+  return (
+    <div>
+      <MeldekortHeader />
+      <Sideinnhold tittel={t("overskrift.tidligereMeldekort")} innhold={innhold} />
+      <Outlet />
+    </div>
+  );
+}
--- app/utils/datoUtils.ts
+++ app/utils/datoUtils.ts
@@ -1,4 +1,4 @@
-import { format, getISOWeek } from "date-fns";
+import { format, getISOWeek, parse } from "date-fns";
 
 export const formaterDato = (dato: Date | string): string => {
   return format(new Date(dato), "dd.MM.yyyy");
@@ -16,4 +16,44 @@ export function formaterPeriodeTilUkenummer(fraOgMed: string, tilOgMed: string) 
   const sluttUkenummer = getISOWeek(new Date(tilOgMed));
 
   return `${startUkenummer} - ${sluttUkenummer}`;
+}
+
+export function formaterPeriodekodeTilDatoer(periodekode: string) {
+  return `${formaterPeriodekodeTilFom(periodekode)} - ${formaterPeriodekodeTilTom(periodekode)}`;
+}
+
+export function formaterPeriodekodeTilFom(periodekode: string) {
+  let aar = periodekode.substring(0, 4)
+  let uke = Number(periodekode.substring(4))
+
+  let fom = new Date(aar);
+  let numOfdaysPastSinceLastMonday = fom.getDay() - 1;
+  fom.setDate(fom.getDate() - numOfdaysPastSinceLastMonday);
+  fom.setDate(fom.getDate() + (7 * (uke - getWeek(fom))));
+
+  return formaterDato(fom)
+}
+
+export function formaterPeriodekodeTilTom(periodekode: string) {
+  let aar = periodekode.substring(0, 4)
+  let uke = Number(periodekode.substring(4))
+
+  let tom = new Date(aar);
+  let numOfdaysPastSinceLastMonday = tom.getDay() - 1;
+  tom.setDate(tom.getDate() - numOfdaysPastSinceLastMonday);
+  tom.setDate(tom.getDate() + (7 * (uke - getWeek(tom))));
+  tom.setDate(tom.getDate() + 14);
+
+  return formaterDato(tom)
+}
+
+function getWeek(input: Date) {
+  let date = new Date(input.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  let week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
 }
