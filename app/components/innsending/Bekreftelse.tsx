641f370f255b34c71cb5fb15bbf1e83884a42ddf
Igor Weber <igor.shuliakov@nav.no>
2024-01-08 21:52:33 +0100

    Bekreftelse komponent

--- app/components/innsending/Bekreftelse.tsx
+++ app/components/innsending/Bekreftelse.tsx
@@ -1,35 +1,111 @@
+import type { ISporsmal } from "~/models/sporsmal";
+import type { Jsonify } from "@remix-run/server-runtime/dist/jsonify";
+import type { IMeldekort } from "~/models/meldekort";
+import { Innsendingstype } from "~/models/innsendingstype";
+import { finnYtelsestypePostfix } from "~/utils/meldekortUtils";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import type { IFravaerInnsending, IMeldekortdetaljerInnsending } from "~/models/meldekortdetaljerInnsending";
+import { FravaerTypeInnsending, sendInnMeldekort } from "~/models/meldekortdetaljerInnsending";
 import { Alert, Box, Button, ConfirmationPanel } from "@navikt/ds-react";
 import { formatHtmlMessage } from "~/utils/intlUtils";
 import Begrunnelse from "~/components/begrunnelse/Begrunnelse";
 import SporsmalOgSvar from "~/components/sporsmalOgSvar/SporsmalOgSvar";
 import Ukeliste from "~/components/ukeliste/Ukeliste";
 import { RemixLink } from "~/components/RemixLink";
-import type { ISporsmal } from "~/models/sporsmal";
-import { useState } from "react";
 
 interface IProps {
   begrunnelse: string;
   sporsmal: ISporsmal;
-  fom: string;
-  ytelsestypePostfix: string;
+  valgtMeldekort: Jsonify<IMeldekort>;
+  innsendingstype: Innsendingstype;
+  melekortApiUrl: string;
   forrigeOnclickHandler: Function;
   nesteOnclickHandler: Function;
 }
 
 export default function Bekreftelse(props: IProps) {
-  const { begrunnelse, sporsmal, fom, ytelsestypePostfix, forrigeOnclickHandler, nesteOnclickHandler } = props
+  const {
+    begrunnelse,
+    sporsmal,
+    valgtMeldekort,
+    innsendingstype,
+    melekortApiUrl,
+    forrigeOnclickHandler,
+    nesteOnclickHandler
+  } = props
+
+  const fom = valgtMeldekort.meldeperiode.fra
+  const ytelsestypePostfix = finnYtelsestypePostfix(valgtMeldekort.meldegruppe)
 
   const { t } = useTranslation(fom)
 
   const [bekreftet, setBekreftet] = useState(false)
   const [visFeil, setVisFeil] = useState(false)
 
+  const hentFravaersdager = () => {
+    const fravar: IFravaerInnsending[] = [];
+    sporsmal.meldekortDager.forEach(meldekortDag => {
+      if (Number(meldekortDag.arbeidetTimerSum) > 0) {
+        fravar.push({
+          dagIndeks: meldekortDag.dag,
+          type: { typeFravaer: FravaerTypeInnsending.ARBEIDS_FRAVAER },
+          arbeidTimer: meldekortDag.arbeidetTimerSum,
+        });
+      }
+
+      if (meldekortDag.syk) {
+        fravar.push({
+          dagIndeks: meldekortDag.dag,
+          type: { typeFravaer: FravaerTypeInnsending.SYKDOM },
+        });
+      }
+
+      if (meldekortDag.kurs) {
+        fravar.push({
+          dagIndeks: meldekortDag.dag,
+          type: { typeFravaer: FravaerTypeInnsending.KURS_UTDANNING },
+        });
+      }
+
+      if (meldekortDag.annetFravaer) {
+        fravar.push({
+          dagIndeks: meldekortDag.dag,
+          type: { typeFravaer: FravaerTypeInnsending.ANNET_FRAVAER },
+        });
+      }
+    });
+    return fravar;
+  }
+
   const valider = () => {
     if (!bekreftet) {
       setVisFeil(true)
     } else {
-      nesteOnclickHandler()
+      // Send
+      const meldekortdetaljer: IMeldekortdetaljerInnsending = {
+        begrunnelse: begrunnelse,
+        erArbeidssokerNestePeriode: !!sporsmal.arbeidssoker,
+        fravaersdager: hentFravaersdager(),
+        korrigerbart: innsendingstype !== Innsendingstype.KORRIGERING,
+        kortStatus: valgtMeldekort.kortStatus,
+        kortType: valgtMeldekort.kortType,
+        meldegruppe: valgtMeldekort.meldegruppe,
+        meldekortId: valgtMeldekort.meldekortId,
+        meldeperiode: valgtMeldekort.meldeperiode,
+        mottattDato: new Date(),
+        sesjonsId: "IKKE I BRUK",
+        signatur: true, // Vi sender ikke uten brukerens samtykke
+        sporsmalsobjekter: []
+      }
+      sendInnMeldekort(melekortApiUrl, meldekortdetaljer)
+        .then(response => {
+          if (response.ok) nesteOnclickHandler()
+          else {
+            console.log(response.status + " " + response.statusText)
+          }
+        })
+        .catch((error) => console.log(error))
     }
   }
 
