854df10355f5e673c256b17e1a58f25cde992a45
Igor Weber <igor.shuliakov@nav.no>
2024-01-11 08:58:27 +0100

    Fikset isbot import

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,7 +2,7 @@ import { PassThrough } from "node:stream";
 import type { EntryContext } from "@remix-run/node";
 import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
+import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
 import { createInstance } from "i18next";
 import i18next from "./i18next.server";
