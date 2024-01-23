dd378002f17afa7fe804e16dd17ba528d4a886ed
Igor Weber <igor.shuliakov@nav.no>
2024-01-23 15:21:55 +0100

    getText

--- app/utils/intlUtils.tsx
+++ app/utils/intlUtils.tsx
@@ -1,4 +1,20 @@
 import type { ReactElement } from "react";
+import i18next from "i18next";
+
+export const getText = (key: string, values?: object): string => {
+  let text = ""
+
+  if (i18next.exists(key)) text = i18next.t(key).trim()
+  else text = i18next.t(key.split("-")[0]).trim()
+
+  if (values) {
+    for (const key in values) {
+      text = text.replace("{" + key + "}", (values as any)[key])
+    }
+  }
+
+  return text
+}
 
 export const parseHtml = (text: string, values?: Array<string>): ReactElement => {
   if (values) {
