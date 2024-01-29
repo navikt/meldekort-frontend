e0b848020306f8670fcecbaa3442f11a82d44825
Igor Weber <igor.shuliakov@nav.no>
2024-01-29 16:22:21 +0100

    Meny for mobile enheter

--- app/components/meldekortHeader/MeldekortHeader.tsx
+++ app/components/meldekortHeader/MeldekortHeader.tsx
@@ -1,11 +1,19 @@
 import { NavLink } from "@remix-run/react";
 import { useExtendedTranslation } from "~/utils/intlUtils";
 import classNames from "classnames";
+import { MenuHamburgerIcon } from "@navikt/aksel-icons";
 import styles from "./MeldekortHeader.module.css";
 
 type LinkState = {
   isActive: boolean,
   isPending: boolean
+}
+
+function togleMobileMenu() {
+  const element = document.getElementById("menu")
+
+  if (element?.classList.contains(styles.open)) element?.classList.remove(styles.open)
+  else element?.classList.add(styles.open)
 }
 
 function setClassName(linkState: LinkState) {
@@ -21,8 +29,11 @@ export default function MeldekortHeader() {
         <div className={styles.title}>
           <h1 className="navds-heading navds-heading--xlarge">{tt("overskrift.meldekort")}</h1>
         </div>
+        <div className={styles.mobileMenu} onClick={() => togleMobileMenu()}>
+          <MenuHamburgerIcon title="a11y-title" fontSize="3rem" />
+        </div>
       </div>
-      <div className={styles.menu}>
+      <div className={styles.menu} id="menu">
         <NavLink to="/send-meldekort" className={setClassName}>
           {tt("sekundarmeny.send")}
         </NavLink>
