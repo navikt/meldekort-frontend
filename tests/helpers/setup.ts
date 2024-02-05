72743f0d867da7a57502d9c48805423eaec5efb4
Igor Weber <igor.shuliakov@nav.no>
2024-02-05 16:04:31 +0100

    Setter MELDEKORT_API_URL

--- tests/helpers/setup.ts
+++ tests/helpers/setup.ts
@@ -1,2 +1,8 @@
 export const TEST_PORT = 8079
 export const TEST_URL = "http://localhost:" + TEST_PORT
+export const MELDEKORT_API_URL = "https://meldekort-api"
+
+if (typeof window !== "undefined") {
+  window.env = window.env || {}
+  window.env.MELDEKORT_API_URL = MELDEKORT_API_URL
+}
