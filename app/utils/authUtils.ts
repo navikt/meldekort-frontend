024021106923082b283203042828897de9e6c3f7
Igor Weber <igor.shuliakov@nav.no>
2024-02-07 22:32:47 +0100

    authUtils code beautifying

--- app/utils/authUtils.ts
+++ app/utils/authUtils.ts
@@ -3,19 +3,12 @@ import { idporten } from "@navikt/dp-auth/identity-providers";
 import { tokenX, withInMemoryCache } from "@navikt/dp-auth/obo-providers";
 import { getEnv } from "~/utils/envUtils";
 
-const fallbackToken =
+export const FALLBACK_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 export let getSession: GetSessionWithOboProvider
 
-if (process.env.IS_LOCALHOST === "true") {
-  getSession = makeSession({
-    identityProvider: async () => process.env.MELDEKORT_API_TOKEN || fallbackToken,
-    oboProvider: process.env.MELDEKORT_API_TOKEN
-      ? tokenX
-      : async (token: string, audience: string) => token + audience,
-  })
-} else {
+if (process.env.IS_LOCALHOST !== "true") {
   getSession = makeSession({
     identityProvider: idporten,
     oboProvider: withInMemoryCache(tokenX),
@@ -24,7 +17,7 @@ if (process.env.IS_LOCALHOST === "true") {
 
 export async function getOboToken(request: Request) {
   if (process.env.IS_LOCALHOST === "true") {
-    return process.env.MELDEKORT_API_TOKEN || fallbackToken
+    return process.env.MELDEKORT_API_TOKEN || FALLBACK_TOKEN
   }
 
   const session = await getSession(request)
