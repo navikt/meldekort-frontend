7925b8f4c5683c6f20afac0d7356e7b9053e830e
Igor Weber <igor.shuliakov@nav.no>
2024-02-07 15:38:28 +0100

    miscUtils code beautifying

--- app/utils/miscUtils.ts
+++ app/utils/miscUtils.ts
@@ -2,33 +2,30 @@ import type { ISporsmal } from "~/models/sporsmal";
 import { getText } from "~/utils/intlUtils";
 
 export function formaterBelop(belop?: number): string {
-  if (typeof belop === "number") {
-    if (belop === 0) {
-      return "kr. 0"
-    }
-    const desimaler = 2
-    const desimalSeparator = ","
-    const tusenSeparator = " "
-    const i = parseInt(
-      Math.abs(Number(belop) || 0).toFixed(desimaler),
-      10
-    ).toString()
-    const j = i.length > 3 ? i.length % 3 : 0
-
-    return (
-      "kr. " +
-      (j ? i.substring(0, j) + tusenSeparator : "") +
-      i.substring(j).replace(/(\d{3})(?=\d)/g, "$1" + tusenSeparator) +
-      (desimaler
-        ? desimalSeparator +
-        Math.abs(belop! - Number(i))
-          .toFixed(desimaler)
-          .slice(2)
-        : "")
-    )
-  } else {
+  if (!belop || belop === 0) {
     return "kr. 0"
   }
+
+  const desimaler = 2
+  const desimalSeparator = ","
+  const tusenSeparator = " "
+  const i = parseInt(
+    Math.abs(Number(belop) || 0).toFixed(desimaler),
+    10
+  ).toString()
+  const j = i.length > 3 ? i.length % 3 : 0
+
+  return (
+    "kr. " +
+    (j ? i.substring(0, j) + tusenSeparator : "") +
+    i.substring(j).replace(/(\d{3})(?=\d)/g, "$1" + tusenSeparator) +
+    (desimaler
+      ? desimalSeparator +
+      Math.abs(belop! - Number(i))
+        .toFixed(desimaler)
+        .slice(2)
+      : "")
+  )
 }
 
 export function byggBegrunnelseObjekt(str: string) {
