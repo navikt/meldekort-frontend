ef83c998658766cca4cd1cb015d55a849e0dfdf1
Igor Weber <igor.shuliakov@nav.no>
2024-02-08 17:17:49 +0100

    meldekortdetaljer.test

--- tests/models/meldekortdetaljer.test.ts
+++ tests/models/meldekortdetaljer.test.ts
@@ -1,37 +1,40 @@
 import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
 import { catchErrorResponse } from "../helpers/response-helper";
-import { hentHistoriskeMeldekort } from "~/models/meldekort";
 import { server } from "../mocks/server";
 import { http, HttpResponse } from "msw";
 import { TEST_MELDEKORT_API_URL } from "../helpers/setup";
-import { jsonify, TEST_HISTORISKEMELDEKORT } from "../mocks/data";
+import { jsonify, TEST_MELDEKORTDETALJER } from "../mocks/data";
+import { hentMeldekortdetaljer } from "~/models/meldekortdetaljer";
 
 // Kan ikke kjøres parallelt!
-describe("Meldekort", () => {
+describe("Meldekortdetaljer", () => {
+
   beforeAll(() => server.listen({ onUnhandledRequest: "error" }))
   afterAll(() => server.close())
   afterEach(() => server.resetHandlers())
 
-  test("hentHistoriskeMeldekort skal få status 500 når feil i backend", async () => {
+  const meldekortId = "1707156949"
+
+  test("hentMeldekortdetaljer skal få status 500 når feil i backend", async () => {
     server.use(
       http.get(
-        `${TEST_MELDEKORT_API_URL}/person/historiskemeldekort`,
+        `${TEST_MELDEKORT_API_URL}/meldekort/1707156949`,
         () => new HttpResponse(null, { status: 500 }),
         { once: true }
       )
     )
 
-    const historiskeMeldekortResponse = await catchErrorResponse(() => hentHistoriskeMeldekort(""))
+    const historiskeMeldekortResponse = await catchErrorResponse(() => hentMeldekortdetaljer("", meldekortId))
 
     expect(historiskeMeldekortResponse.status).toBe(500)
     expect(historiskeMeldekortResponse.statusText).toBe("Internal Server Error")
   })
 
-  test("hentHistoriskeMeldekort skal få data", async () => {
-    const expectedData = [...TEST_HISTORISKEMELDEKORT] // Clone
+  test("hentMeldekortdetaljer skal få data", async () => {
+    const expectedData = { ...TEST_MELDEKORTDETALJER } // Clone
     jsonify(expectedData)
 
-    const historiskeMeldekortResponse = await hentHistoriskeMeldekort("")
+    const historiskeMeldekortResponse = await hentMeldekortdetaljer("", meldekortId)
 
     const historiskeMeldekort = await historiskeMeldekortResponse.json()
 
@@ -39,11 +42,11 @@ describe("Meldekort", () => {
     expect(historiskeMeldekort).toStrictEqual(expectedData)
   })
 
-  test("hentHistoriskeMeldekort skal få status 500 når feil i fetch", async () => {
+  test("hentMeldekortdetaljer skal få status 500 når feil i fetch", async () => {
     // Stopper server slik at fetch kaster exception
     server.close()
 
-    const historiskeMeldekortResponse = await catchErrorResponse(() => hentHistoriskeMeldekort(""))
+    const historiskeMeldekortResponse = await catchErrorResponse(() => hentMeldekortdetaljer("", meldekortId))
 
     expect(historiskeMeldekortResponse.status).toBe(500)
     expect(historiskeMeldekortResponse.statusText).toBe("fetch failed")
