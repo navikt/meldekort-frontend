95827bef79173e3bc3872bee140cd0bd3b638d5d
Igor Weber <igor.shuliakov@nav.no>
2024-02-09 16:03:21 +0100

    ofte-stilte-sporsmal.test

---
+++ tests/routes/ofte-stilte-sporsmal.test.tsx
@@ -0,0 +1,43 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import OfteStilteSporsmal, { meta } from "~/routes/ofte-stilte-sporsmal";
+import type { ServerRuntimeMetaArgs } from "@remix-run/server-runtime/dist/routeModules";
+
+
+describe("Ofte stilte spørsmål", () => {
+  test("Skal vise informasjon", async () => {
+    const testRouter = createBrowserRouter([
+      {
+        path: "/",
+        element: <OfteStilteSporsmal />,
+        action: async (args: any) => {
+          return args
+        }
+      }
+    ]);
+
+    render(<RouterProvider router={testRouter} />)
+
+    expect(screen.getByText("oss.sende.overskrift")).toBeDefined()
+    expect(screen.getByText("oss.sende.tekst")).toBeDefined()
+    expect(screen.getByText("oss.frist.overskrift")).toBeDefined()
+    expect(screen.getByText("oss.frist.tekst")).toBeDefined()
+    expect(screen.getByText("oss.korrigere.overskrift")).toBeDefined()
+    expect(screen.getByText("oss.korrigere.tekst")).toBeDefined()
+    expect(screen.getByText("oss.pengene.overskrift")).toBeDefined()
+    expect(screen.getByText("oss.pengene.tekst")).toBeDefined()
+    expect(screen.getByText("oss.utbetalt.overskrift")).toBeDefined()
+    expect(screen.getByText("oss.utbetalt.tekst")).toBeDefined()
+    expect(screen.getByText("overskrift.ofteStilteSporsmal")).toBeDefined()
+  })
+
+  test("Skal returnere metainformasjon", async () => {
+    const args = {} as ServerRuntimeMetaArgs
+
+    expect(meta(args)).toStrictEqual([
+      { title: "Meldekort" },
+      { name: "description", content: "Ofte stilte spørsmål" },
+    ])
+  })
+})
