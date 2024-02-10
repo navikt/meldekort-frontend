9ea0dcaeed58b75d6d5502100449f08fa61edad1
Igor Weber <igor.shuliakov@nav.no>
2024-02-10 18:42:10 +0100

    UtvidetInformasjon.test

---
+++ tests/components/UtvidetInformasjon.test.tsx
@@ -0,0 +1,27 @@
+import { describe, test } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import UtvidetInformasjon from "~/components/utvidetInformasjon/UtvidetInformasjon";
+
+describe("UtvidetInformasjon", () => {
+  test("Skal vide innhold og åpnes og lukkes", async () => {
+    render(<UtvidetInformasjon innhold={<div>INNHOLD</div>} />)
+
+    // Sjekk at det vises en lenke som brukes til å åpne info
+    const linkLes = await waitFor(() => screen.findByText("veiledning.les"))
+
+    // Klikk på lenken
+    linkLes.click()
+
+    // Sjekker at det vises innhold
+    await waitFor(() => screen.findByText("INNHOLD"))
+
+    // Sjekk at det vises en lenke som brukes til å lukke info
+    const linkLukk = await waitFor(() => screen.findByText("veiledning.lukk"))
+
+    // Klikk på lenken
+    linkLukk.click()
+
+    // Sjekk at det igjen vises en lenke som brukes til å åpne info
+    await waitFor(() => screen.findByText("veiledning.les"))
+  })
+})
