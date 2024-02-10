b40fa3a17849e44a038c721c01b96f3913726cb8
Igor Weber <igor.shuliakov@nav.no>
2024-02-10 17:57:34 +0100

    window.scrollTo() for tester

--- tests/helpers/setup.ts
+++ tests/helpers/setup.ts
@@ -7,4 +7,5 @@ if (typeof window !== "undefined") {
   window.env = window.env || {}
   window.env.MELDEKORT_API_URL = TEST_MELDEKORT_API_URL
   window.env.MIN_SIDE_URL = TEST_MIN_SIDE_URL
+  window.scrollTo = () => {}
 }
