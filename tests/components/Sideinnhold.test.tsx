3bd15db660db570091250cf7c43dde9f6c4f4f2b
Igor Weber <igor.shuliakov@nav.no>
2024-02-11 14:57:05 +0100

    Sideinnhold.test

---
+++ tests/components/Sideinnhold.test.tsx
@@ -0,0 +1,34 @@
+import { afterEach, describe, expect, test } from "vitest";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import Sideinnhold from "~/components/sideinnhold/Sideinnhold";
+
+describe("Sideinnhold", () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  const tittel = "TITTEL"
+  const innhold = "INNHOLD"
+  const innholdElement = <div>{innhold}</div>
+
+  test("Skal vise innhold med tittel", async () => {
+    render(<Sideinnhold tittel={tittel} innhold={innholdElement} />)
+
+    await waitFor(() => screen.findByText(tittel))
+    await waitFor(() => screen.findByText(innhold))
+  })
+
+  test("Skal vise innhold uten tittel", async () => {
+    render(<Sideinnhold tittel={""} innhold={innholdElement} />)
+
+    await waitFor(() => screen.findByText(innhold))
+  })
+
+  test("Skal vise innhold uten tittel når utenSideoverskrift = true", async () => {
+    render(<Sideinnhold tittel={"TITTEL"} innhold={innholdElement} utenSideoverskrift={true} />)
+
+    const tittelElement = await waitFor(() => screen.queryByText(tittel))
+    expect(tittelElement).toBeNull()
+    await waitFor(() => screen.findByText(innhold))
+  })
+})
