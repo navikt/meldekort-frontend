1407ab7d014626ecf915622f970e02c85c5c4bb4
Igor Weber <igor.shuliakov@nav.no>
2024-02-14 08:39:44 +0100

    Innsending.3.Bekreftelse.test

---
+++ tests/components/Innsending.3.Bekreftelse.test.tsx
@@ -0,0 +1,50 @@
+import { afterEach, describe, test } from "vitest";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import { Innsendingstype } from "~/models/innsendingstype";
+import { jsonify, opprettTestMeldekort, TEST_SPORSMAL } from "../mocks/data";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import * as React from "react";
+import Bekreftelse from "~/components/innsending/3-Bekreftelse";
+import type { Jsonify } from "@remix-run/server-runtime/dist/jsonify";
+import type { IMeldekort } from "~/models/meldekort";
+
+
+describe("Bekreftelse", () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  test("Skal vise innhold for Korrigering", async () => {
+    const valgtMeldekort = opprettTestMeldekort(1707696000)
+    jsonify(valgtMeldekort)
+
+    createRouteAndRender((valgtMeldekort as unknown) as Jsonify<IMeldekort>, Innsendingstype.KORRIGERING)
+
+    await waitFor(() => screen.findByText("overskrift.steg3.info.ikkeSendt"))
+    await waitFor(() => screen.findByText("overskrift.steg3.info.bekreftVerdier"))
+    await waitFor(() => screen.findByText("korrigering.sporsmal.begrunnelse"))
+  })
+})
+
+const createRouteAndRender = (
+  valgtMeldekort: Jsonify<IMeldekort>,
+  innsendingstype: Innsendingstype,
+  nesteMeldekortKanSendes: string | undefined = undefined
+) => {
+  const testRouter = createMemoryRouter([
+    {
+      path: "/",
+      element: <Bekreftelse begrunnelse={""}
+                            sporsmal={TEST_SPORSMAL}
+                            valgtMeldekort={valgtMeldekort}
+                            innsendingstype={innsendingstype}
+                            activeStep={3}
+                            setActiveStep={() => {
+                            }}
+                            nesteMeldekortKanSendes={nesteMeldekortKanSendes}
+      />
+    }
+  ])
+
+  render(<RouterProvider router={testRouter} />)
+}
