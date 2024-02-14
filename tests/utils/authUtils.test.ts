47750474474ccd8d33974e4b078c5db863c1a154
Igor Weber <igor.shuliakov@nav.no>
2024-02-14 21:59:33 +0100

    authUtils.test sjekker at vi får token når session ikke er null

--- tests/utils/authUtils.test.ts
+++ tests/utils/authUtils.test.ts
@@ -4,10 +4,28 @@ import { catchErrorResponse } from "../helpers/response-helper";
 
 
 describe("Auth utils", () => {
+  vi.mock("@navikt/oasis", () => {
+    return {
+      makeSession: () => {
+        return (request: Request) => {
+          if (request.url === "http://uten.session/") {
+            return null
+          }
+
+          return {
+            apiToken: () => {
+              return "API TOKEN"
+            }
+          }
+        }
+      }
+    }
+  })
+
   test("getOboToken skal returnere FALLBACK_TOKEN på localhost uten MELDEKORT_API_TOKEN", async () => {
     vi.stubEnv("IS_LOCALHOST", "true")
 
-    const result = await getOboToken(new Request("http://localhost"))
+    const result = await getOboToken(new Request("http://localhost/"))
     expect(result).toBe(FALLBACK_TOKEN)
   })
 
@@ -15,15 +33,22 @@ describe("Auth utils", () => {
     vi.stubEnv("IS_LOCALHOST", "true")
     vi.stubEnv("MELDEKORT_API_TOKEN", "testToken")
 
-    const result = await getOboToken(new Request("http://localhost"))
+    const result = await getOboToken(new Request("http://localhost/"))
     expect(result).toBe("testToken")
   })
 
   test("getOboToken skal returnere feil uten session", async () => {
     vi.stubEnv("IS_LOCALHOST", "false")
 
-    const result = await catchErrorResponse(() => getOboToken(new Request("http://localhost")))
+    const result = await catchErrorResponse(() => getOboToken(new Request("http://uten.session/")))
     expect(result.status).toBe(500)
     expect(result.statusText).toBe("Feil ved henting av sesjon")
+  })
+
+  test("getOboToken skal returnere API token", async () => {
+    vi.stubEnv("IS_LOCALHOST", "false")
+
+    const token = await getOboToken(new Request("http://localhost/"))
+    expect(token).toBe("API TOKEN")
   })
 })
