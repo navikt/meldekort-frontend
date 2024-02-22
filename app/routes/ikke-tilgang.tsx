11f39adaa374c40d7881bceb533504a109905297
Igor Weber <igor.shuliakov@nav.no>
2024-02-22 13:27:42 +0100

    ikke-tilgang

---
+++ app/routes/ikke-tilgang.tsx
@@ -0,0 +1,25 @@
+import type { MetaFunction } from "@remix-run/node";
+import Sideinnhold from "~/components/sideinnhold/Sideinnhold";
+import { Alert, Heading } from "@navikt/ds-react";
+import { parseHtml, useExtendedTranslation } from "~/utils/intlUtils";
+
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Meldekort" },
+    { name: "description", content: "Du har ikke tilgang" }
+  ]
+}
+
+export default function IkkeTilgang() {
+  const { tt } = useExtendedTranslation()
+
+  const alert = <Alert variant="error">
+    <Heading spacing size="small" level="3">{parseHtml(tt("ikke.tilgang.overskrift"))}</Heading>
+    {parseHtml(tt("ikke.tilgang.tekst"))}
+  </Alert>
+
+  return (
+    <Sideinnhold utenSideoverskrift={true} innhold={alert} />
+  )
+}
