68b78a251cf356ae91b2dbe790193095a4ca5625
Igor Weber <igor.shuliakov@nav.no>
2024-03-01 15:11:53 +0100

    LoaderMedPadding

---
+++ app/components/LoaderMedPadding.tsx
@@ -0,0 +1,12 @@
+import { Box, Loader } from '@navikt/ds-react';
+
+
+export default function LoaderMedPadding() {
+  return (
+    <div>
+      <Box padding="32" />
+      <Loader size="3xlarge" className="img" title="Venter..." />
+      <Box padding="32" />
+    </div>
+  );
+}
