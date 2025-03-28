1c7757d833d88edc636c49564d42711b4ce00f8b
Igor Weber <igor.shuliakov@nav.no>
2025-03-28 12:53:46 +0100

    application/json

--- app/utils/aapUtils.ts
+++ app/utils/aapUtils.ts
@@ -1,14 +1,12 @@
+import { getHeaders } from "~/utils/fetchUtils";
+
 export async function hentHarAAP(onBehalfOfToken: string): Promise<Response> {
   const url = 'http://meldekort-backend.aap/api/ansvarlig-system-felles';
 
   try {
     return await fetch(url, {
       method: "GET",
-      headers: {
-        "Accept": "text/plain",
-        "Content-Type": "text/plain",
-        "Authorization": `Bearer ${onBehalfOfToken}`
-      },
+      headers: getHeaders(onBehalfOfToken)
     });
   } catch (err) {
     const response = new Response(null, { status: 500, statusText: (err as Error).message });
