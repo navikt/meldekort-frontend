17cff11a8829937b743ed690479a3c8aca0516ee
Igor Weber <igor.weber@nav.no>
2025-12-18 10:07:59 +0100

    skrivemodus true

--- app/routes/tidligere-meldekort_.tsx
+++ app/routes/tidligere-meldekort_.tsx
@@ -39,7 +39,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
   }
 
   skrivemodus = {
-    skrivemodus: false
+    skrivemodus: true
   }
   return { feil, skrivemodus, historiskeMeldekort };
 }
