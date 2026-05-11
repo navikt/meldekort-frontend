5cd8ee0c07050893cc8bdfe9b26d1f1596f0c09e
Igor Weber <85676385+igorweber@users.noreply.github.com>
2026-05-11 09:48:55 +0200

    Nye majors (#811)

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { reactRouter } from "@react-router/dev/vite";
 import path from "path";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   build: {
-    target: "ESNext",
+    target: "esnext",
   },
-  base: process.env.NODE_ENV === "production" ? "https://cdn.nav.no/meldekort/meldekort-frontend/client/" : "/felles-meldekort",
-  plugins: [
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  base:
+    process.env.NODE_ENV === "production"
+      ? "https://cdn.nav.no/meldekort/meldekort-frontend/client/"
+      : "/felles-meldekort",
+  plugins: [reactRouter()],
   server: {
-    port: 8080
+    port: 8080,
   },
   resolve: {
+    tsconfigPaths: true,
     alias: {
       "~": path.resolve(__dirname, "./app"),
     },
